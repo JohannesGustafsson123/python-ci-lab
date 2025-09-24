54adbd54ac4c3db563591638e20dc88af0075d03
Johannes Gustafsson <johannes.gustafsson@elev.ga.ntig.se>
2025-09-24 20:04:02 +0200

    Fix  E712 Avoid equality comparisons to `True`

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,31 +1,31 @@
 from palinlib import is_palindrome
 
 def test_palindrome_simple_odd():
-    assert is_palindrome("abcba") == True
+    assert is_palindrome("abcba")
 
 def test_palindrome_simple_even():
-    assert is_palindrome("abba") == True
+    assert is_palindrome("abba")
 
 def test_palindrome_simple_odd_negative():
-    assert is_palindrome("abcca") == False
+    assert not is_palindrome("abcca")
 
 def test_palindrome_simple_even_negative():
-    assert is_palindrome("abca") == False
+    assert not is_palindrome("abca")
 
 def test_palindrome_simple_longer_odd():
-    assert is_palindrome("abcdefghijkjihgfedcba") == True
+    assert is_palindrome("abcdefghijkjihgfedcba")
 
 def test_palindrome_simple_longer_even():
-    assert is_palindrome("abcdefghijjihgfedcba") == True
+    assert is_palindrome("abcdefghijjihgfedcba")
 
 def test_palindrome_empty():
-    assert is_palindrome("") == True
+    assert is_palindrome("")
 
 def test_palindrome_one_letter():
-    assert is_palindrome("") == True
+    assert is_palindrome("")
 
 def test_palindrome_two_letters():
-    assert is_palindrome("aa") == True
+    assert is_palindrome("aa")
 
 def test_palindrome_two_letters_negative():
-    assert is_palindrome("ab") == False
+    assert not is_palindrome("ab")
